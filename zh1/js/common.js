3f3edf860d01d5ad47fd14c5b020a4ce936e1925
summerNa <1031363398@qq.com>
2018-11-07 23:40:28 +0800

    提交

---
+++ zh1/js/common.js
@@ -0,0 +1,36 @@
+var isLoginS = localStorage.getItem("vrbig");
+if (isLoginS) {
+  isLogin = JSON.parse(localStorage.getItem("vrbig"));
+  if(Object.keys(isLogin).length) {
+    $('.usernameAndLogin').attr('href', 'javascript:;');
+    $('.usernameAndLogin').html(isLogin.phone.substr(0, 3) + '****' + isLogin.phone.substr(7));
+    $('.logoutAndRegister').html('退出');
+    $('.logoutAndRegister').attr('href', 'javascript:;');
+  }
+  $('.logoutAndRegister').on('click', function() {
+    if (Object.keys(isLogin).length) {
+      // 登出
+      localStorage.setItem("vrbig", "");
+      $(location).attr('href', 'home.html');  // 跳转到首页
+    }
+  });
+}
+
+$('.click_im_area').on('click', function() {
+  if (isLoginS) {
+    $(location).attr('href', 'import.html');  // 跳转到首页
+  } else {
+    $(location).attr('href', 'login.html');  // 跳转到首页
+  }
+});
+
+$('.header_wrap .header .list').on('click', function() {
+  if (isLoginS) {
+    $(location).attr('href', 'list.html');  // 跳转到首页
+  } else {
+    $(location).attr('href', 'login.html');  // 跳转到首页
+  }
+});
+
+
+
